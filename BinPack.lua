6904015b083184485196e24a4acad844e4f12d95
lnx00 <linch255@gmail.com>
2023-03-02 20:36:27 +0100

    Added unpacking + Test

--- BinPack.lua
+++ BinPack.lua
@@ -1,16 +1,41 @@
+--[[
+    BinPack.lua
+    A simple binary packing library for Lua
+    Author: LNX (github.com/lnx00)
+]]
+
 ---@class BinPack
 local BinPack = {}
 
+-- Packs multiple byte strings into a single byte string
 ---@vararg string
 function BinPack.pack(...)
     local args = { ... }
     local result = {}
 
     for _, arg in ipairs(args) do
+        table.insert(result, string.char(string.len(arg)))
         table.insert(result, arg)
     end
 
     return table.concat(result)
+end
+
+-- Unpacks a packed byte string into multiple byte strings
+---@param data string
+---@return table
+function BinPack.unpack(data)
+    local result = {}
+    local offset = 1
+
+    while offset <= string.len(data) do
+        local length = string.byte(data, offset)
+        local value = string.sub(data, offset + 1, offset + length)
+        table.insert(result, value)
+        offset = offset + length + 1
+    end
+
+    return result
 end
 
 return BinPack
--- Test.lua
+++ Test.lua
@@ -2,10 +2,19 @@ local BinPack = require("BinPack")
 
 local data1<const> = "\00\01\02\03\04"
 local data2<const> = "\05\06\07\08\09"
+local expected<const> = "\5\0\1\2\3\4\5\5\6\a\b\t"
 
 -- Pack the sample data
 do
     local result = BinPack.pack(data1, data2)
-    assert(result == "\00\01\02\03\04\05\06\07\08\09", "Packed data does not match expected result")
+    assert(result == expected)
     print("Packed data successfully")
+end
+
+-- Unpack the sample data
+do
+    local result = BinPack.unpack(expected)
+    assert(result[1] == data1)
+    assert(result[2] == data2)
+    print("Unpacked data successfully")
 end
